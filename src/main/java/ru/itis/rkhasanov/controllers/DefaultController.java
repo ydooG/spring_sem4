4cd71f1d4116622d7d5e3be1bc755ea9504ebc84
Rasim <rasim.2@bk.ru>
2020-03-04 20:59:47 +0300

    Fixed: Tomcat can't see views

--- src/main/java/ru/itis/rkhasanov/controllers/DefaultController.java
+++ src/main/java/ru/itis/rkhasanov/controllers/DefaultController.java
@@ -1,4 +1,16 @@
 package ru.itis.rkhasanov.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/main")
 public class DefaultController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String main() {
+        return "main";
+    }
 }
